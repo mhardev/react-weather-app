050b248cedaa5b4c47e67c8362921b09a4f40100
Joshua Mhar Alcubilla <103411030+mhardev@users.noreply.github.com>
2025-07-28 02:04:47 +0800

    Refactoring HorizontallyScrollable function and Forecast CSS components.

--- src/components/HorizontallyScrollable.js
+++ src/components/HorizontallyScrollable.js
@@ -5,30 +5,53 @@ function HorizontallyScrollable({ children, className='' }) {
     const scrollRef = useRef();
 
     const handleMouseDown = (e) => {
-        const oldX = e.pageX;
-        const scrollLeft = scrollRef.current.scrollLeft;
-        
-        const handleMouseMove = (e) => {
-            const newX = e.pageX;
-            const offset = newX - oldX;
+    e.preventDefault();
+    const startX = e.pageX;
+    const scrollLeft = scrollRef.current.scrollLeft;
 
-            scrollRef.current.scrollLeft = scrollLeft - offset;
-        };
+    const handleMouseMove = (e) => {
+      const x = e.pageX;
+      const walk = x - startX;
+      scrollRef.current.scrollLeft = scrollLeft - walk;
+    };
 
+    const handleMouseUp = () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
 
-        const handleMouseUp = () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp)
-        }
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  };
 
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mouseup', handleMouseUp)
-    }
+  const handleTouchStart = (e) => {
+    const startX = e.touches[0].pageX;
+    const scrollLeft = scrollRef.current.scrollLeft;
+
+    const handleTouchMove = (e) => {
+      const x = e.touches[0].pageX;
+      const walk = x - startX;
+      scrollRef.current.scrollLeft = scrollLeft - walk;
+    };
+
+    const handleTouchEnd = () => {
+      window.removeEventListener("touchmove", handleTouchMove);
+      window.removeEventListener("touchend", handleTouchEnd);
+    };
+
+    window.addEventListener("touchmove", handleTouchMove);
+    window.addEventListener("touchend", handleTouchEnd);
+  };
+
   return (
-    <div className={className} ref={scrollRef} onMouseDown={handleMouseDown}>
+    <div
+      className={className}
+      ref={scrollRef}
+      onMouseDown={handleMouseDown}
+      onTouchStart={handleTouchStart}
+      style={{ overflowX: 'auto', WebkitOverflowScrolling: 'touch' }} // Important for mobile
+    >
       {children}
     </div>
-  )
+  );
 }
-
-export default HorizontallyScrollable
